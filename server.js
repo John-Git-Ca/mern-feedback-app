f92177d917dd48ade9b7077b56d8f2e56531593c
John <john.lizuhua@outlook.com>
2022-01-12 16:09:02 -0500

    update server.js with express.static and res.sendfile

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const connectDB = require('./database/database.js')
 const routes = require('./routes/routes.js')
 const dotenv = require('dotenv')
+const path = require("path")
 
 dotenv.config()
 
@@ -14,11 +15,14 @@ connectDB()
 app.use(express.json())
 app.use(cors())
 app.use('/', routes)
+app.use(express.static(path.join(__dirname, "client", "build")))
 
 app.get('/', (req, res) => {
   res.json('hello there')
 })
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 app.listen(port, ()=>{
   console.log(`app is listening on port ${port}...`)
 })
