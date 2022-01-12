37b1c88e1a48bb66807a9d33d4d5cf304f028f9f
John <john.lizuhua@outlook.com>
2022-01-12 16:22:04 -0500

    update try catch in the server

--- controllers/feedbackController.js
+++ controllers/feedbackController.js
@@ -1,25 +1,39 @@
 const Feedback = require('../model/feedback.js')
 
 const postFeedback = async (req, res) =>{
-  const {text, rating} = req.body
-
-  const feedback = new Feedback({
-    text: text,
-    rating: rating,
-  })
-
-  const createdFeedback = await feedback.save()
-  res.status(201).json(createdFeedback)
+  try {
+    const {text, rating} = req.body
+  
+    const feedback = new Feedback({
+      text: text,
+      rating: rating,
+    })
+  
+    const createdFeedback = await feedback.save()
+    res.status(201).json(createdFeedback)
+  } catch (error) {
+    res.status(404).send('error when posting')
+  }
 }
 
 const getFeedback = async (req, res) => {
-  const feedbacks = await Feedback.find({})
-  res.json(feedbacks)
+  try {
+    
+    const feedbacks = await Feedback.find({})
+    res.json(feedbacks)
+  } catch (error) {
+    res.send('error when getting')
+  }
 }
 
 const deleteFeedback = async (req, res) => {
-  await Feedback.deleteOne({_id: req.params.id})
-  res.json({message: 'feedback removed'})
+  try {
+    
+    await Feedback.deleteOne({_id: req.params.id})
+    res.json({message: 'feedback removed'})
+  } catch (error) {
+    res.send('error when deleting')
+  }
 }
 
 module.exports = {
