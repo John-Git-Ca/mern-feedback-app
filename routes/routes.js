d65a3e0a02cf2fdf3574e1192be8293a0c45281b
John <john.lizuhua@outlook.com>
2022-01-12 17:12:57 -0500

    comment out the mongoose and text  again

--- routes/routes.js
+++ routes/routes.js
@@ -7,8 +7,22 @@ const {
   deleteFeedback,
 } = require('../controllers/feedbackController.js')
 
-router.route('/post').post(postFeedback)
-router.route('/get').get(getFeedback)
-router.route('/delete/:id').delete(deleteFeedback)
+const postingFeedback = (req, res) =>{
+  res.send('posting')
+}
+const gettingFeedback = (req, res) =>{
+  res.send('getting')
+}
+
+const deletingFeedback = (req, res) => {
+  res.send('deleting')
+}
+
+// router.route('/post').post(postFeedback)
+// router.route('/get').get(getFeedback)
+// router.route('/delete/:id').delete(deleteFeedback)
+router.route('/post').post(postingFeedback)
+router.route('/get').get(gettingFeedback)
+router.route('/delete/:id').delete(deletingFeedback)
 
 module.exports = router
